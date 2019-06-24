e2d5e14457d0ecb2116189dbbf25950e55d4d6d4
Lucas Diniz <49011583+LucasCdiniz@users.noreply.github.com>
2019-06-24 18:54:45 -0300

    Add files via upload

---
+++ Triangulo.cs
@@ -0,0 +1,10 @@
+﻿namespace UdemyPrimeirosProjetos
+{
+    class Triangulo
+    {
+        // nome de Atributo com o nome em maiúsculo
+        public double A;
+        public double B;
+        public double C;
+    }
+}
